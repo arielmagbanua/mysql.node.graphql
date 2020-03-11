ecf0ffc5fa60eaf50190187826524813e1c7342b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-11 23:25:58 +0800

    Basic graphql set up.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const graphqlHttp = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
 // routes
 const applicationRoutes = require('./routes/app');
 
@@ -11,5 +15,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(applicationRoutes);
+
+app.use('/graphql', graphqlHttp({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+}));
 
 app.listen(8080);
