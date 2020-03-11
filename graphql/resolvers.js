bf16a799135dc7bc473367713e937d7f17aa172a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-11 23:38:05 +0800

    Basic static product query.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,4 +5,24 @@ module.exports = {
       views: 900,
     };
   },
+  products() {
+    return [
+      {
+        sku: 'PROD-1',
+        description: 'Kamote Hash Browns',
+        title: 'Kamote Hash Browns',
+        comment: 'This is the comment',
+        price: 300.55,
+        weight: 500,
+      },
+      {
+        sku: 'PROD-2',
+        description: 'Patatas Hash Browns',
+        title: 'Patatas Hash Browns',
+        comment: 'This is the comment',
+        price: 30.55,
+        weight: 50,
+      },
+    ];
+  },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,15 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+  type Product {
+    sku: ID!
+    description: String
+    title: String!
+    comment: String
+    price: Float!
+    weight: Float!
+  }
+  
   type TestData {
     text: String!
     views: Int!
@@ -8,6 +17,7 @@ module.exports = buildSchema(`
   
   type RootQuery {
     hello: TestData!
+    products: [Product]!
   }
   
   schema {
