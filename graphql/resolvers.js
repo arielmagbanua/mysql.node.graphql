e50f08106d74fd8159304382c9ee6ce691a307ab
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-12 23:14:22 +0800

    Added querying of all products.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product');
+
 module.exports = {
   hello() {
     return {
@@ -5,24 +7,10 @@ module.exports = {
       views: 900,
     };
   },
-  products() {
-    return [
-      {
-        sku: 'PROD-1',
-        description: 'Kamote Hash Browns',
-        title: 'Kamote Hash Browns',
-        comment: 'This is the comment',
-        price: 300.55,
-        weight: 500,
-      },
-      {
-        sku: 'PROD-2',
-        description: 'Patatas Hash Browns',
-        title: 'Patatas Hash Browns',
-        comment: 'This is the comment',
-        price: 30.55,
-        weight: 50,
-      },
-    ];
+  async products() {
+    return Product.findAll().then((products) => {
+      console.log(products);
+      return products;
+    });
   },
 };
