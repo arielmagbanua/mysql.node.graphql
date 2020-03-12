3b7afcfe0e02e1eab0994d206e80c8c78fe6a067
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-13 00:28:47 +0800

    Added product search

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,11 @@
 const Product = require('../models/product');
 
 module.exports = {
+  async product({ fields }, req) {
+    return Product.findOne({
+      where: { ...fields },
+    });
+  },
   async products() {
     return Product.findAll();
   },
