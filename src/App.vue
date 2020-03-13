d422d660e866a8bd04859d33b525583efd1d0be4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-14 01:38:50 +0800

    Successfully integrated materializecss.

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,30 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <a class="waves-effect waves-light btn-large">Button</a>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import { M } from 'materialize-css/dist/js/materialize.min';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
   },
+  mounted() {
+    // eslint-disable-next-line new-cap
+    M.AutoInit();
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import '~materialize-css/dist/css/materialize.min.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
