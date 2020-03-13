3dcbd12bbdfec1c67aaa83e50826b7e757c737e3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-14 00:29:15 +0800

    Added deleteProductBySKU resolver.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,6 +2,15 @@ const Product = require('../models/product');
 const { v5: uuidv5 } = require('uuid');
 
 module.exports = {
+  async deleteProductBySKU({ sku }, req) {
+    const product = await Product.findByPk(sku);
+    if (product) {
+      const deleted = await product.destroy();
+      return !!deleted;
+    }
+
+    return false;
+  },
   async product({ fields }, req) {
     return Product.findOne({
       where: { ...fields },
