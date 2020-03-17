e2c84b974c81fd0ad2fc6d5c311b3eb1df94101b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-17 22:23:26 +0800

    Added model binding for each form fields.

--- src/components/AddProductForm.vue
+++ src/components/AddProductForm.vue
@@ -6,65 +6,65 @@
       <div class="container-fluid">
         <div class="row">
           <div class="input-field col s6">
-            <input id="title" type="text" class="validate">
+            <input id="title" v-model="title" type="text" class="validate">
             <label class="active" for="title">Title</label>
           </div>
           <div class="input-field col s6">
-            <input id="price" type="number" class="validate">
+            <input id="price" v-model="price" type="number" class="validate">
             <label class="active" for="price">Price</label>
           </div>
           <div class="input-field col s12">
-            <input id="thumb" type="text" class="validate">
+            <input id="thumb" v-model="thumb" type="text" class="validate">
             <label class="active" for="thumb">Thumbnail</label>
           </div>
           <div class="input-field col s12">
-            <input id="image" type="text" class="validate">
+            <input id="image" v-model="image" type="text" class="validate">
             <label class="active" for="image">Image</label>
           </div>
           <div class="input-field col s12">
-            <input id="image_large" type="text" class="validate">
+            <input id="image_large" v-model="imageLarge" type="text" class="validate">
             <label class="active" for="image_large">Image Large</label>
           </div>
           <div class="input-field col s12">
-            <input id="url" type="text" class="validate">
+            <input id="url" v-model="url" type="text" class="validate">
             <label class="active" for="url">URL</label>
           </div>
           <div class="input-field col s12">
-            <textarea id="description" class="materialize-textarea" data-length="120"></textarea>
+            <textarea id="description" v-model="description" class="materialize-textarea" data-length="120"></textarea>
             <label for="description">Description</label>
           </div>
           <div class="input-field col s12">
-            <textarea id="comment" class="materialize-textarea" data-length="240"></textarea>
+            <textarea id="comment" v-model="comment" class="materialize-textarea" data-length="240"></textarea>
             <label for="comment">Comment</label>
           </div>
           <div class="input-field col s3">
-            <input id="sale_price" type="number" class="validate">
+            <input id="sale_price" v-model="salePrice" type="text" class="validate">
             <label class="active" for="sale_price">Sale Price</label>
           </div>
           <div class="input-field col s3">
-            <input id="prod_discount" type="number" class="validate">
+            <input id="prod_discount" v-model="productDiscount" type="number" class="validate">
             <label class="active" for="prod_discount">Discount</label>
           </div>
           <div class="input-field col s3">
-            <input id="product_type" type="text" class="validate">
+            <input id="product_type" v-model="productType" type="text" class="validate">
             <label class="active" for="product_type">Product Type</label>
           </div>
           <div class="input-field col s3">
-            <input id="weight" type="text" class="validate">
+            <input id="weight" v-model="weight" type="text" class="validate">
             <label class="active" for="weight">Weight</label>
           </div>
           <div class="input-field col s3">
-            <input id="size" type="text" class="validate">
+            <input id="size" v-model="size" type="text" class="validate">
             <label class="active" for="size">Size</label>
           </div>
           <div class="input-field col s3">
-            <input id="color" type="text" class="validate">
+            <input id="color" v-model="color" type="text" class="validate">
             <label class="active" for="color">Color</label>
           </div>
           <div class="input-field col s3">
             <p>
               <label>
-                <input id="inactive" type="checkbox" class="filled-in" checked="checked" />
+                <input id="inactive" v-model="inactive" type="checkbox" class="filled-in"/>
                 <span>Inactive</span>
               </label>
             </p>
@@ -94,6 +94,21 @@ export default {
   data() {
     return {
       modal: null,
+      title: '',
+      price: '',
+      thumb: '',
+      image: '',
+      imageLarge: '',
+      url: '',
+      description: '',
+      comment: '',
+      salePrice: null,
+      productDiscount: 0.0,
+      productType: '',
+      weight: null,
+      size: null,
+      color: null,
+      inactive: false,
     };
   },
   methods: {
