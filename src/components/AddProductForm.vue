c40ddb476aa4df358c1f5643f63af85b107a8141
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-03-18 21:00:50 +0800

    Finished the adding of product.

--- src/components/AddProductForm.vue
+++ src/components/AddProductForm.vue
@@ -22,7 +22,7 @@
             <label class="active" for="image">Image</label>
           </div>
           <div class="input-field col s12">
-            <input id="image_large" v-model="imageLarge" type="text" class="validate">
+            <input id="image_large" v-model="image_large" type="text" class="validate">
             <label class="active" for="image_large">Image Large</label>
           </div>
           <div class="input-field col s12">
@@ -34,19 +34,19 @@
             <label for="description">Description</label>
           </div>
           <div class="input-field col s12">
-            <textarea id="comment" v-model="comment" class="materialize-textarea" data-length="240"></textarea>
-            <label for="comment">Comment</label>
+            <textarea id="comment_right" v-model="comment_right" class="materialize-textarea" data-length="240"></textarea>
+            <label for="comment_right">Comment</label>
           </div>
           <div class="input-field col s3">
-            <input id="sale_price" v-model="salePrice" type="text" class="validate">
+            <input id="sale_price" v-model="sale_price" type="text" class="validate">
             <label class="active" for="sale_price">Sale Price</label>
           </div>
           <div class="input-field col s3">
-            <input id="prod_discount" v-model="productDiscount" type="number" class="validate">
+            <input id="prod_discount" v-model="prod_discount" type="number" class="validate">
             <label class="active" for="prod_discount">Discount</label>
           </div>
           <div class="input-field col s3">
-            <input id="product_type" v-model="productType" type="text" class="validate">
+            <input id="product_type" v-model="product_type" type="text" class="validate">
             <label class="active" for="product_type">Product Type</label>
           </div>
           <div class="input-field col s3">
@@ -89,25 +89,31 @@
 
 <script>
 import M from 'materialize-css/dist/js/materialize.min';
+import axios from 'axios';
 
 export default {
+  props: {
+    addProduct: {
+      type: Function,
+    },
+  },
   data() {
     return {
       modal: null,
       title: '',
-      price: '',
+      price: 0.00,
       thumb: '',
       image: '',
-      imageLarge: '',
+      image_large: '',
       url: '',
       description: '',
-      comment: '',
-      salePrice: null,
-      productDiscount: 0.0,
-      productType: '',
-      weight: null,
-      size: null,
-      color: null,
+      comment_right: '',
+      sale_price: '',
+      prod_discount: 0.0,
+      product_type: '',
+      weight: '',
+      size: '',
+      color: '',
       inactive: false,
     };
   },
@@ -118,8 +124,74 @@ export default {
     close() {
       this.modal.close();
     },
+    formatValue(key, value) {
+      if (key === 'price' || key === 'prod_discount') {
+        return `${parseFloat(value)}`;
+      }
+
+      if (key === 'inactive') {
+        return `${value ? 1 : 0}`;
+      }
+
+      if (typeof this[key] === 'string') {
+        return `"${value}"`;
+      }
+
+      return `${value}`;
+    },
     save() {
-      // TODO: Save the product here
+      let input = '';
+
+      // build the input
+      for (const [key, value] of Object.entries(this.$data)) {
+        // do not include the modal
+        if (key === 'modal') {
+          continue;
+        }
+
+        input += `${key}: ${this.formatValue(key, value)}\n`;
+      }
+      console.log(input);
+      const createPayload = {
+        query: `
+          mutation {
+            createProduct(productInput: {
+              ${input}
+            }) {
+              sku
+              description
+              title
+              thumb
+              image
+              price
+              size
+              weight
+              url
+              sale_price
+              prod_discount
+              inactive
+            }
+          }
+        `,
+      };
+
+      axios({
+        method: 'post',
+        url: 'http://localhost:8823/graphql',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(createPayload),
+      })
+        .then((response) => {
+          const { createdProduct } = response.data.data;
+          this.addProduct(createdProduct);
+
+          M.toast({
+            html: `${createdProduct.title} is added!`,
+            classes: 'rounded',
+          });
+        });
 
       this.modal.close();
     },
